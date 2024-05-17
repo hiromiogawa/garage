4de3282ab8ae44af8d2e91a988cf5886c582c6ff
hiromi <hiromi.ogawa.1223@gmail.com>
2024-05-17 21:34:55 +0900

    feat: 🎸 Add .env.development

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // 左から順に環境変数が呼ばれる
+      envFilePath: ['.env.development', '.env'],
+      load: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
